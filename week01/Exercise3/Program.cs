63cf2b8b37b36a86ba1ca90d5a9c4759923f9bad
tiatuihalangingie <tiafaanunu@gmail.com>
2025-01-13 21:20:23 -0700

    Exercise Three

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,40 @@
-using System;
+using System. Collections.Generic;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Hello World! This is the Exercise3 Project.");  
+        
+       
+        Console.Write("What is the magic number? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        string response = "number";
+
+        while (response == "number")
+        {
+            Console.Write("What is your guess? ");
+            string valueFromuser = Console.ReadLine();
+            int quess = int.Parse(valueFromuser);
+    
+            if (quess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (quess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                response = Console.ReadLine();
+
+            }
+            
+        }
     }
 }
