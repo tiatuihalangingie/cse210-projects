fd3613280acd72ba07b58fc7ebb8f569c6636169
tiatuihalangingie <tiafaanunu@gmail.com>
2025-01-14 16:31:34 -0700

    Exercise 4 Week One

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+    
+        List<int> numbers = new List<int>();
+        
+        
+        while (number == 0)
+        {
+            Console.Write("Enter a number: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }    
+        }
+        
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int maxNumber = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
+        Console.ReadKey();
+    
     }
 }
