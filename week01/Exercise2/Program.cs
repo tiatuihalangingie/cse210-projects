01fe8e84d500db79f5ce8d9403d307102cfebac1
tiatuihalangingie <tiafaanunu@gmail.com>
2025-01-14 14:09:58 -0700

    Exercise 2 in Week One

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,42 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
 
-        Console.Write("Enter your grade : ");
+        Console.Write("Enter your grade percentage: ");
         string valueFromUser = Console.ReadLine();
-
-        int x = int.Parse(valueFromUser);
+        int x = int.Parse(valueFromUser);   
         int a = 90; 
-        int b = 80; 
-        int c = 70;
-        int d = 61;
-
+        int b = 80;     
+        int c = 70; 
+        int d = 60; 
+ 
         if (x > a)
         {
             Console.WriteLine("A");
+            Console.WriteLine("Well Done!");
         }
-
-        if (x > b)
+         if (x >= b && x < a)
         {
             Console.WriteLine("B");
+            Console.WriteLine("Good Job!");
         }
-        
-        else if (x > c)
+        else if (x >= c && x < b)
         {
             Console.WriteLine("C");
+            Console.WriteLine("You made it!");
         }
-        else if (x > d)
+        else if (x >= d && x < c)
         {
             Console.WriteLine("D");
-    
+            Console.WriteLine("You almost make it!");
         }
-        else
+        else if (x < d)
         {
             Console.WriteLine("F");
-            Console.WriteLine("Please try harder next time");
+            Console.WriteLine("Please try harder next time!");
         }
-    
          
     }
 }
