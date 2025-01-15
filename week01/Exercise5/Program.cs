3fba93ad01fd02601827c477ebfa135813184575
tiatuihalangingie <tiafaanunu@gmail.com>
2025-01-14 23:24:06 -0700

    week01/Exercise5/Program.cs

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.IO.Compression;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+
+        string firstName = PromptUsername();
+        string lastName = PromptUsername();
+
+        
+    
     }
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welocome to the Program!");
+    }
+
+    static void PromptUsername()
+    {
+        Console.WriteLine($"Please enter your name: ");
+        string name
+        
+    }    
+    
+    static int favoriteNumbers(int number);
+    {
+        Console.WriteLine($"Please enter your favorite number: {number}");
+    }
+    
+    static void squareNumber(  )
+        
+        Brother Burton, the square of your number is 1764
+    
+    
 }
